cf9bba638f4804896dac745b1848324c5024e196
Joyce <joycelin79@gmail.com>
2020-06-27 19:34:18 -0700

    add route

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/', async (req, res) => {
+    function isOverLimit(thingToCheck) {
+      // to define
+    }
+    // check rate limit
+    let overLimit = await isOverLimit(req.ip)
+    if (overLimit) {
+      res.status(429).send('Too many requests - try again later')
+      return
+    }
+    // allow access to whatever resources you want
+    res.send("Accessed my precious")
+  })
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
